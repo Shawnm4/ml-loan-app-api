eee9a2195c910d032c15ddc174b0f2b32be2281e
shawnm4 <shawnm18057310@gmail.com>
2024-05-23 01:11:58 -0400

    simplifying for testing

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 from flask import Flask,request,jsonify
-import predictor
+# import predictor
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 @app.route("/",methods=["POST"])
 def home():
+    return jsonify()
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    # data=request.get_json(force=True)
+    # features = data.get('features',[])
+    
+    # # prediction = predictor.predictor(features)
+    # response ={
+    #     'prediction':prediction,
+    #     'recieved_feats':features
+    # }
     return jsonify({
         'test':"test",
         'recieved_feats':"test"
     })
-
-@app.route("/predict",methods=["POST"])
-def predict():
-    data=request.get_json(force=True)
-    features = data.get('features',[])
-    
-    prediction = predictor.predictor(features)
-    response ={
-        'prediction':prediction,
-        'recieved_feats':features
-    }
-    return jsonify(response)
 
 @app.route('/favicon.ico')
 def favicon():
