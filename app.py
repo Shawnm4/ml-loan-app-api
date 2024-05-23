3e620903f7dd1b245f6541dfbc8b2d481f0d2fac
shawnm4 <shawnm18057310@gmail.com>
2024-05-23 00:50:17 -0400

    fixed a build issue in requirements

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 CORS(app)
 @app.route("/")
 def home():
-    return "Goodbye World"
+    return jsonify("Goodbye World")
 
 @app.route("/predict",methods=["POST"])
 def predict():
